20ec80881dc07669eee700d0d3819d8e71493efe
Daniel V. Egdal <daniel.v.egdal@gmail.com>
2023-09-27 22:45:54 +0200

    other hash

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod cube;
 use std::collections::VecDeque;
-use std::collections::HashSet;
-use std::collections::HashMap;
+// use std::collections::HashSet;
+// use std::collections::HashMap;
+use rustc_hash::FxHashMap;
+use rustc_hash::FxHashSet;
 
 use cube::Cube;
 
@@ -17,14 +19,14 @@ fn gen_all_dr(){
         };
     let moves:Vec<u8> = vec![1, 2, 3, 4, 11, 12, 13, 14, 15, 16, 21, 22, 23, 24];
 
-    let mut states: [HashSet<Cube>; 14] = Default::default();
+    let mut states: [FxHashSet<Cube>; 14] = Default::default();
 
     states[0].insert(c);
 
     // println!("{:?}",states);
     for idx in 0..states.len()-1{
         println!("Optimal: {}. States: {}",idx,states[idx].len());
-        let mut next_states: HashSet<Cube> = HashSet::new();
+        let mut next_states: FxHashSet<Cube> = FxHashSet::default();
         for state in &states[idx] {
             for movee in &moves{
                 let new_state = &state.perform_move(*movee);
@@ -52,7 +54,7 @@ fn other_dr(){
         edges: 74381997087264900,
     };
 
-    let mut set: HashSet<Cube> = HashSet::new();
+    let mut set: FxHashSet<Cube> = FxHashSet::default();
     let moves:Vec<u8> = vec![1, 2, 3, 4, 11, 12, 13, 14, 15, 16, 21, 22, 23, 24];
     let mut q:VecDeque<Cube> = VecDeque::from(vec![c]);
     let mut i = 0;
@@ -62,10 +64,11 @@ fn other_dr(){
             println!("{},{}",i, set.len())
         }
         // if !set.contains(&nc){
-            set.insert(nc);
+            // set.insert(nc);
             for movee in &moves{
                 let new_state = &nc.perform_move(*movee);
                 if !set.contains(&new_state){
+                    set.insert(nc);
                     q.push_back(*new_state);
                 }
             }
